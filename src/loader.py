50c700d394e1b0a241025b62eb4fede0debdd8f0
Tijana Lazic <tijanaticalazic02@gmail.com>
2024-02-06 21:50:34 +0100

    Izmene u loader.py

--- src/loader.py
+++ src/loader.py
@@ -3,10 +3,15 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-def load_all_tables(plot=False):
+def load_all_tables(plot=False, years=[x for x in range(2011, 2021)], file='co'):
     dfs = []
-    for year in range(2011, 2021):
-        dfs.append(pd.read_csv(f'..\\data\\{year}\\{year}-co_max8h.csv'))
+    for year in years:
+        try:
+            dfs.append(pd.read_csv(f'data\\{year}\\{year}-{file}.csv'))
+        finally:
+            continue
+    
+    print(dfs)
 
     for df in dfs:
         df['Datum'] = pd.to_datetime(df['Datum'], format='%Y-%m-%d')
