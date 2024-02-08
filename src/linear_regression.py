db21ae392366f75695d5d8cf84447ae0e3370b05
Tijana Lazic <tijanaticalazic02@gmail.com>
2024-02-08 18:33:36 +0100

    Linearna regresija

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -2,67 +2,57 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from utils import *
 import matplotlib.pyplot as plt
+from loader import *
+from utils import *
 
 
-matplotlib.rcParams['figure.figsize'] = (8, 3)
-sb.set(font_scale=1.)
+def making_table(location_name):
+    # spajam tabele o zagadjenju za 2019. i 2020. godinu
+    df = load_all_tables(years = [2019, 2020], file = 'pm2.5')
+    for stanica in df:
+        df = fillna_mean(df, stanica) 
+    #print(df)
 
-# NO2C:data\linreg\NO2.csv
-#zadatak 1
-df = pd.read_csv('data\linreg\NO2.csv', sep = ',')
-df.head()
+    location = pd.read_csv(f'data\LinReg\{location_name}.csv')
 
-df = df.dropna()
+    location.insert(0, 'pm2.5', list(df[location_name]))
+    location = location.set_index('datetime')
+    #print(location)
+    return location
 
-x = df.drop(columns=['Kotlarnica Nova fabrika Jagodina'])
-y = df['Kotlarnica Nova fabrika Jagodina']
-model = get_fitted_model(x, y)
 
-df_test = pd.read_csv('data\linreg\testNO2.csv', sep=',')
-x_test = df_test.drop(columns=['AirPollution'])
-y_test = df_test['AirPollution']
+def linear_regression(location):
+        
+    x = location.drop(columns=['pm2.5'])
+    y = location['pm2.5']
+    model = get_fitted_model(x, y)
+    print(model.summary())
+    
+    location = location.drop(columns=['sealevelpressure', 'windspeed'])
+    x = location.drop(columns=['pm2.5'])
+    y = location['pm2.5']
+    x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=42, shuffle=True)
+    model = get_fitted_model(x_train, y_train)
+    print(model.summary())
+    print(are_assumptions_satisfied(model,x_train,y_train))
 
-test_rmse = get_rsquared(model, x_test, y_test)
-print(f'rmse: {test_rmse}')
-'''
-#zadatak 2
-min_expected_raise, max_expected_raise = get_conf_interval(model,'IndustrialEmissions', alpha=0.05)
-print(f'min_expected_raise: {min_expected_raise: .2f}')
-print(f'max_expected_raise: {max_expected_raise: .2f}')
+    test_rmse = get_rmse(model,x_test,y_test)
+    print(f'test rmse: {test_rmse:.2f}')
+    
+    # ax = plt.figure().add_subplot(projection='3d')
 
-autocorrelation, _ = independence_of_errors_assumption(model, sm.add_constant(x), y, plot=False)
-if autocorrelation is None:
-    print('vrednosti su validen jer je zadovoljena pretpostavka o nezavisnosti greske')
-else:
-    print('vrednosti nisu validne')
+    # x = np.linspace(0, 250, 731)
+    # y = 78.2366 - 2.0604 * np.array(list(location['temp'])) - 0.3218 * np.array(list(location['humidity']))
+    # ax.plot(x, y, zs=0, zdir='z', label='curve in (x, y)')
 
-#zadatak 3
-df = pd.read_csv('data/train.csv')
-print(check_for_missing_values(df))
+    # colors = ('r', 'g', 'b', 'k')
+    
+    # ax.scatter(location['temp'], location['humidity'], zs=location['pm2.5'])
 
-print(model.summary())
+    # ax.legend()
+    # ax.set_xlabel('temp')
+    # ax.set_ylabel('humidity')
+    # ax.set_zlabel('pm2.5')
 
-#ovde sa dmoram da nagadjam
-df['TrafficDensity'] = df['TrafficDensity'].interpolate(method='linear', limit_direction='both')
+    # plt.show()
 
-#dropovanje
-df = df.drop(columns=['WindSpeed', 'Temperature', 'GreenSpace', 'Wind'])
-
-#perfect_collinearity_assumption(df, True)
-
-x = df.drop(columns=['AirPollution'])
-y = df['AirPollution']
-x_train, x_val, y_train, y_val = train_test_split(x, y, train_size=0.8, random_state=42, shuffle=True)
-
-model = get_fitted_model(x_train, y_train)
-print(are_assumptions_satisfied(model,x_train,y_train))
-
-val_rmso = get_rmse(model, x_val, y_val)
-print(f'validation rmse:{val_rmso: .2f}')
-
-df_test = pd.read_csv('data/test.csv', sep=',')
-x_test = df_test.drop(columns=['AirPollution','WindSpeed', 'Temperature', 'GreenSpace', 'Wind'])
-y_test = df_test['AirPollution']
-test_rmse = get_rmse(model,x_test,y_test)
-print(f'test rmse: {test_rmse:.2f}')
-'''
